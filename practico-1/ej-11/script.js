015f0db82b205293ec02dd505841099bcc135abe
fgblackpony-windows <134664813+fgblackpony@users.noreply.github.com>
2023-08-25 13:55:35 -0300

    Modificar ejercicio 11 (const IVA = .23)

--- practico-1/ej-11/script.js
+++ practico-1/ej-11/script.js
@@ -1,6 +1,7 @@
 document.querySelector("button").addEventListener("click", () => {
   const subTotal = Number(document.querySelector("#importe").value);
-  const total = subTotal * 1.23;
+  const IVA = 0.23;
+  const total = subTotal * (1 + IVA);
 
   document.querySelector("#p1").innerHTML = `El monto total es: ${total.toFixed(
     2
