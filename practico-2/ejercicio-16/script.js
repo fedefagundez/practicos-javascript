2a795ecf7c2372be2d1ada0acd3df3ffd0230942
fgblackpony <134664813+fgblackpony@users.noreply.github.com>
2023-09-04 16:35:57 -0300

    Hacer ejercicios 17 y 18

---
+++ practico-2/ejercicio-16/script.js
@@ -0,0 +1,9 @@
+let a, b, c;
+a = 7;
+b = 2;
+c = 4;
+
+if (a > b && a > c) {
+    document.querySelector("#parrafo").innerHTML = "a es el mayor de los tres.";
+}
+
---
+++ practico-2/ejercicio-17/script.js
@@ -0,0 +1,10 @@
+let a, b, c;
+a = -4;
+b = 2;
+c = 4;
+
+if(a<0 || a > 10){
+    document.querySelector("#parrafo").innerHTML = "a está fuera de rango";
+}
+
+
